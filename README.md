2d4f52958d7c76bb18f809f9dcfc3d802a7a18c5
Martan03 <martinslezak03@gmail.com>
2025-10-09 10:18:52 +0200

    Initial commit (cmake doesn't work :))

---
+++ README.md
@@ -0,0 +1,16 @@
+# terragl
+
+## Table of Contents
+{{ mdcon }}
+
+## Installation
+
+## Usage
+
+## Detailed description
+
+## Links
+
+- **Author:** [Martan03](https://github.com/Martan03)
+- **GitHub repository:** [terragl](https://github.com/Martan03/terragl)
+- **Author website:** [martan03.github.io](https://martan03.github.io)
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW\n";
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "TerraGL Test", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create GLFW window\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  gladLoadGL(glfwGetProcAddress);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << "\n";
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
