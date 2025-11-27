4f638c71e43c6d7f69286eef3c67eb330dfbd175
Martan03 <martinslezak03@gmail.com>
2025-11-27 22:45:41 +0100

    Add perlin derivative gen

--- src/height_map/perlin.cpp
+++ src/height_map/perlin.cpp
@@ -31,4 +31,41 @@ float Perlin::noise(float x, float y) {
     return lerp(x1, x2, v);
 }
 
+glm::vec3 Perlin::deriv_noise(float x, float y) {
+    int xi = (int)std::floor(x) & 255;
+    int yi = (int)std::floor(y) & 255;
+
+    x -= std::floor(x);
+    y -= std::floor(y);
+
+    float u = fade(x), v = fade(y);
+    float du = d_fade(x), dv = d_fade(y);
+
+    int a = _perm[xi] + yi;
+    glm::vec2 g00 = get_grad(_perm[a]);
+    glm::vec2 g01 = get_grad(_perm[a + 1]);
+    float p00 = g00.x * x + g00.y * y;
+    float p01 = g01.x * x + g01.y * (y - 1);
+
+    int b = _perm[xi + 1] + yi;
+    glm::vec2 g10 = get_grad(_perm[b]);
+    glm::vec2 g11 = get_grad(_perm[b + 1]);
+    float p10 = g10.x * (x - 1) + g10.y * y;
+    float p11 = g11.x * (x - 1) + g11.y * (y - 1);
+
+    float x1 = lerp(p00, p10, u);
+    float x2 = lerp(p01, p11, u);
+    float val = lerp(x1, x2, v);
+
+    float gx1 = lerp(g00.x, g10.x, u) + du * (p10 - p00);
+    float gx2 = lerp(g01.x, g11.x, u) + du * (p11 - p01);
+    float dx = lerp(gx1, gx2, v);
+
+    float gy1 = lerp(g00.y, g10.y, u);
+    float gy2 = lerp(g01.y, g11.y, u);
+    float dy = lerp(gy1, gy2, v) + dv * (x2 - x1);
+
+    return glm::vec3{ dx, dy, val };
+}
+
 } // namespace tgl::height_map
--- src/height_map/perlin.hpp
+++ src/height_map/perlin.hpp
@@ -4,6 +4,8 @@
 
 #include "noise.hpp"
 
+#include <glm/glm.hpp>
+
 namespace tgl::height_map {
 
 class Perlin : public Noise {
@@ -12,17 +14,28 @@ public:
 
     float noise(float x, float y) override;
 
+    glm::vec3 deriv_noise(float x, float y);
+
 private:
     std::vector<int> _perm;
 
     static float fade(float t) { return t * t * t * (t * (t * 6 - 15) + 10); }
+    static float d_fade(float t) {
+        return 30.0f * t * t * (t * (t - 2.0f) + 1.0f);
+    }
+
     static float lerp(float a, float b, float t) { return a + t * (b - a); }
+
     static float grad(int hash, float x, float y) {
+        auto g = get_grad(hash);
+        return g.x * x + g.y * y;
+    }
+    static glm::vec2 get_grad(int hash) {
         static const int grad2[8][2] = { { 1, 0 },  { -1, 0 }, { 0, 1 },
                                          { 0, -1 }, { 1, 1 },  { -1, 1 },
                                          { 1, -1 }, { -1, -1 } };
         const int *g = grad2[hash & 7];
-        return g[0] * x + g[1] * y;
+        return { g[0], g[1] };
     }
 
     static constexpr int p[256] = {
